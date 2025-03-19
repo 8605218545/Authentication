09aa925f91ef44eda5cff46cf1d2fc313b386236
8605218545 <149319632+8605218545@users.noreply.github.com>
2025-03-19 22:47:15 +0530

    Update Login.jsx

--- frontend/src/LandingPage/User/Login.jsx
+++ frontend/src/LandingPage/User/Login.jsx
@@ -35,7 +35,7 @@ function Login({ setIsLoggedIn }) {
 
         // Connect frontend to backend
         try {
-            const response = await axios.post("https://backend-plra.onrender.com/login", formData, { withCredentials: true });
+            const response = await axios.post("https://backend-km0z.onrender.com/login", formData, { withCredentials: true });
             console.log("Login Response:", response.data);
 
             // Show success toast
