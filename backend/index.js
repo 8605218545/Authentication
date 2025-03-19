ce00afcc1ce6a8bc001d735ed08b3cf353b59c31
8605218545 <149319632+8605218545@users.noreply.github.com>
2025-03-19 22:36:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -57,8 +57,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-
-app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
